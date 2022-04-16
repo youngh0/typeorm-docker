6d5f696f8157fba8d133abc065f80757355d2e6a
Youngho <cyh6099@gmail.com>
2022-04-17 00:30:15 +0900

    test

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send('이제 캡스톤 시작...........................');
+});
+
+app.listen(3000, () => {
+    createConnection().then(async connection => {
+
+        console.log("Inserting a new user into the database...");
+        const user = new User();
+        user.firstName = "daniel";
+        user.lastName = "Saw";
+        user.age = 25;
+        await connection.manager.save(user);
+        console.log("Saved a new user with id: " + user.id);
+    
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+    
+        console.log("Here you can setup and run express/koa/any other framework.");
+    
+    }).catch(error => console.log(error));
+
+    console.log('Start Server with 3000 port');
+});
