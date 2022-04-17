2150d38a40692192c7cd77330910ff94eca26e30
Youngho <cyh6099@gmail.com>
2022-04-17 22:32:31 +0900

    webhook

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/hello', (req,res) => res.send("hello"));
-router.get('/test', (req,res) => res.send("test"))
+router.get('/test', (req,res) => res.send("test"));
+router.get('/test1', (req,res) => res.send("test1"));
 
 export = router;
