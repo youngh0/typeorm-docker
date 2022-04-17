6b9602f00607bce3998495b95b7137d824d3de11
Youngho <cyh6099@gmail.com>
2022-04-17 18:27:24 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 import express, { Request, Response, NextFunction } from 'express';
+import testRouter from './routes/test.route';
 
 const app = express();
 
@@ -10,23 +11,25 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(3000, () => {
-    createConnection().then(async connection => {
+    // createConnection().then(async connection => {
 
-        console.log("Inserting a new user into the database...");
-        const user = new User();
-        user.firstName = "daniel";
-        user.lastName = "Saw";
-        user.age = 25;
-        await connection.manager.save(user);
-        console.log("Saved a new user with id: " + user.id);
+    //     console.log("Inserting a new user into the database...");
+    //     const user = new User();
+    //     user.firstName = "daniel";
+    //     user.lastName = "Saw";
+    //     user.age = 25;
+    //     await connection.manager.save(user);
+    //     console.log("Saved a new user with id: " + user.id);
     
-        console.log("Loading users from the database...");
-        const users = await connection.manager.find(User);
-        console.log("Loaded users: ", users);
+    //     console.log("Loading users from the database...");
+    //     const users = await connection.manager.find(User);
+    //     console.log("Loaded users: ", users);
     
-        console.log("Here you can setup and run express/koa/any other framework.");
+    //     console.log("Here you can setup and run express/koa/any other framework.");
     
-    }).catch(error => console.log(error));
+    // }).catch(error => console.log(error));
 
     console.log('Start Server with 3000 port');
 });
+
+app.use('/test', testRouter)
