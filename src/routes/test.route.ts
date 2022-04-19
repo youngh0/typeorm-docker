01d4464b9f08bb4d1f5a085ab5d96e1560f97aa6
Youngho <cyh6099@gmail.com>
2022-04-19 15:02:59 +0900

    test

--- src/routes/test.route.ts
+++ src/routes/test.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/hello', (req,res) => res.send("hello12"));
+router.get('/hello', (req,res) => res.send("hello123"));
 router.get('/test', (req,res) => res.send("test"));
 router.get('/test1', (req,res) => res.send("test1"));
 router.get('/test2', (req,res) => res.send("test2"));
